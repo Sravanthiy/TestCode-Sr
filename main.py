fdcc4ac8ee1956762805673c8df6c3e3378e2994
Sravanthiy <50407590+Sravanthiy@users.noreply.github.com>
2019-05-08 20:51:34 +0100

    Update main.py
    updated function extra commas to iterate for all extra commas.
    more comments

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ import datetime as dt
 
 
 def fieldSortAndTop(data, h, col=10):
-    # produce a list sorted by Current Rent in ascending order .
+    ##Requirement: produce a list sorted by Current Rent in ascending order .
     ##Obtain the first 5 items from the resultant list and  output to the console.
+    
+    #initialise an empty list
     list1 = []
+    
+    #iterate over data to gather current rent values 
     for j in range(1, len(data), 1):
         try:
             list1.append(float(data[j][h[col]]))
         except:
             print("Error converting Current Rent to float on line", j, "\n", data[j])
     list1 = sorted(set(list1))
+    
+    #collect top5 and print
     top5 = list1[0:5]
     print('\n', '\033[1m' + "Top 5 " + '\033[4m', h[col].strip(), '\033[0m', "are", '\033[0m', top5)
+    
     return list1
 
 
 def filterOnLeaseYears(data, h, y=25, col=9):
-    # From the list of all mast data create new list of mast data with Lease Years = 25 years.
-    # Output the list to the console, include all data fields.
-    # Output the total rent for all items in this list to the console
+    ##Requirement From the list of all mast data create new list of mast data with Lease Years = 25 years.
+    ## Output the list to the console, include all data fields.
+    ## Output the total rent for all items in this list to the console
 
     # initialise variable
     totalrent = 0
@@ -40,13 +47,14 @@ def filterOnLeaseYears(data, h, y=25, col=9):
                 print("Error converting Current Rent to float on line", i, "\n", data[i])
 
     print('\n', '\033[1m', "Total rent is", '\033[0m', totalrent)
+    
     return newmastdata, totalrent
 
 
 def groupByField(data, h, col=6):
     ##Requirement:
-    # Create a dictionary containing tenant name and a count of masts for each tenant.
-    # Output the dictionary to the console in a readable form.
+    ## Create a dictionary containing tenant name and a count of masts for each tenant.
+    ## Output the dictionary to the console in a readable form.
 
     # Create a unique tenants name list
     tenants = []
@@ -66,14 +74,14 @@ def groupByField(data, h, col=6):
         dtnt[tenants[i]] = mastcount
         # Output in readable format
         print(tenants[i], ":", mastcount)
+        
     return dtnt
 
 
 def filterOnLeaseSD(data, h, col=7, d1='01 Jun 1999', d2='31 Aug 2007'):
-    # Requirement:
-    # List the data for rentals with Lease Start Date between 1 June 1999 and 31 Aug 2007.
-    # Output the data to the console with dates formatted as DD/MM/YYYY
-    ##
+    ## Requirement:
+    ## List the data for rentals with Lease Start Date between 1 June 1999 and 31 Aug 2007.
+    ## Output the data to the console with dates formatted as DD/MM/YYYY
 
     # printing header
     print('\n', '\033[1m', "Data filtered on", '\033[4m', h[col], "between", d1, d2, '\033[0m', '\n')
@@ -82,8 +90,9 @@ def filterOnLeaseSD(data, h, col=7, d1='01 Jun 1999', d2='31 Aug 2007'):
     # formatting input strings to dates
     d1 = dt.datetime.strptime(d1, "%d %b %Y")
     d2 = dt.datetime.strptime(d2, "%d %b %Y")
+    
+    # filtered in on dates and creating a new data list with filtered data
     newmastdata = []
-    # filtered in on dates
     for i in range(1, len(data), 1):
         lstartd = dt.datetime.strptime(data[i][h[col]], "%d %b %Y")
 
@@ -92,8 +101,10 @@ def filterOnLeaseSD(data, h, col=7, d1='01 Jun 1999', d2='31 Aug 2007'):
             # reformatting dates
             data[i][h[col]] = lstartd.strftime('%d/%m/%Y')
             data[i][h[col + 1]] = lendd.strftime('%d/%m/%Y')
+            #printing the data output
             print(", ".join(list(data[i].values())))
             newmastdata.append(data[i])
+            
     return newmastdata
 
 
@@ -102,7 +113,9 @@ def all(data, h):
     filterOnLeaseYears(data, h)
     groupByField(data, h)
     filterOnLeaseSD(data, h)
-
+    
+    return None
+    
 
 def choice():
     # Choice text
@@ -116,34 +129,44 @@ def choice():
           "dates formatted as DD/MM/YYYY.")
     print("5. Do all of these.")
     print("6. Exit.")
-
+    
+    return None
 
 def extraComma(line, h):
-    string = re.findall(r'\"(.+?)\"', line)[0]
-    newstring = re.sub(r',', '', string)
-    newline = line.replace(string, newstring)
+    #find the string between double quotes
+    for str in re.findall(r'\"(.+?)\"', line):
+        #remove comma from the above string
+        newstring = re.sub(r',', '', str)
+        #create a line replacing the part with extra comma
+        newline = line.replace(str, newstring)
+    #check if all extra commas have been replaced
     if len(newline.split(",")) == len(h):
         return newline
     else:
         print("Error formatting line", line)
-
+        return None
 
 def processData(file):
+    #split file data in lines
     infile = file.readlines()
+    #read header
     headers = infile[0].strip().split(",")
+    #initialising a datadictionary
     datadict = list(range(1, len(infile) + 1, 1))
-
+    #updatin this diction
     for j in range(1, len(infile), 1):
         list1 = infile[j].strip().split(",")
+        #check for extra commas
         if len(list1) == len(headers):
             pass
         else:   
             new = extraComma(infile[j].strip(), headers)
             list1 = new.split(",")
-
+        #iterate over header list and assign values
         datadict[j] = {}
         for i in range(0, len(headers), 1):
             datadict[j][headers[i]] = list1[i]
+            
     return headers, datadict
 
 
